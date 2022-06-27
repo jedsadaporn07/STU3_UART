0be72949147b5672b34e87ea1c71375025753c00
jedsadaporn07 <jedsadaporn.07@mail.kmutt.ac.th>
2022-06-27 15:24:04 +0700

    UART_Mode_Update

--- STU3_UART/Core/Src/main.c
+++ STU3_UART/Core/Src/main.c
@@ -22,7 +22,8 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include <stdio.h>
-#include "string.h"
+#include <string.h>
+#include <math.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -40,6 +41,8 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim11;
+
 UART_HandleTypeDef huart2;
 DMA_HandleTypeDef hdma_usart2_rx;
 DMA_HandleTypeDef hdma_usart2_tx;
@@ -49,12 +52,28 @@ uint8_t temp_s[2]="Xu";
 uint8_t temp_f[2]="Fn";
 ////////////// transmit parameter ////////////////////
 uint8_t Req_sta[4] = {153,0,5,97}; //////////////// station 5
-uint8_t Req_AngPosi[4] = {154,61,18,22};/////////// 15634 , 1.5634 rad 90 degree
+//////////////////////////////////////////////////////
+//uint8_t Req_AngPosi[4] = {154,0,0,0};
+uint8_t Req_AngPosi[4] = {154,61,18,22};/////////// 15634 , 1.5634 rad 90 degree 6.10865
 //uint8_t Req_AngPosi[4] = {154,122,183,52};/////////// 31415 , 3.1415 rad 180 degree
+int DataProtocol_AngPosi;
+////////////// control///////////////////////////////////
+float KalV = 0.942478; // Velo / rad
+float CurrentEn = 4.27606; // AngPosi / rad
+////////////////////////////////////////////////////////
 uint8_t Req_MaxVelo[4] = {155,0,127,229};////////// 5 rpm
+float DataProtocol_Velo;
+//float DataProtocol_Velo;
 ////////////// Receive parameter ////////////////////
-uint8_t Set_AngVelo[2];
-uint8_t Set_AngPosi[2];
+int Set_AngVelo[2];
+float DataProtocol_SetVelo;
+float AngVelo;
+/////////////////////////////////////////////////////
+int Set_AngPosi[2];
+int DataProtocol_SetAngPosi;
+float AngPosi;
+/////////////////////////////////////////////////////
+int Set1_Sta;
 uint8_t Set_Goal_1Sta[2];
 uint8_t Set_Goal_nSta[10];
 /////////////////////////////////////////////////////
@@ -74,6 +93,14 @@ uint8_t M_state;
 uint8_t Posdata;
 uint8_t Nstation;
 uint8_t dataFSum;
+
+uint64_t _micros = 0;
+uint64_t timestamp = 0;
+uint8_t Ang_Position = 0;
+
+uint16_t encdummbuf = 0;
+
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -81,22 +108,27 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_TIM11_Init(void);
 /* USER CODE BEGIN PFP */
 void All_mode();
 void check_Mode();
+void AngPos_Dummy();
+uint64_t micros();
+void HAL_TIM_PeriodElapsedCallback();
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
 #define RxBuf_SIZE   32
-#define MainBuf_SIZE 32
+#define MainBuf_SIZE 1024
 
 uint8_t RxBuf[RxBuf_SIZE];
 uint8_t MainBuf[MainBuf_SIZE];
 
 uint16_t oldPos = 0;
 uint16_t newPos = 0;
+
 
 void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
 {
@@ -124,6 +156,18 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
 		if (chkStart == 9){
 			if (NameM >= 1 && NameM <= 14){
 				check_Mode();
+			}
+		}
+		else if (StartM == 88) {
+//			newPos = oldPos + 4;
+//			oldPos = newPos;
+			StartM = MainBuf[newPos-2];
+			chkStart = StartM >> 4;
+			NameM = (StartM & 15);
+			if (chkStart == 9){
+				if (NameM >= 1 && NameM <= 14){
+				check_Mode();
+				}
 			}
 		}
 
@@ -173,6 +217,8 @@ void check_Mode()
 					chksum2 = ~(StartM + dataF1 + dataF2);
 				if (chksum == chksum2){
 					M_state = 4;
+					DataProtocol_SetVelo = Set_AngVelo[1];
+					AngVelo = (DataProtocol_SetVelo * 10)/255;
 					//HAL_UART_Transmit_DMA(&huart2, (uint8_t*)temp_s, 2);
 					HAL_UART_Transmit(&huart2, (uint8_t*)temp_s, 2 ,1000); //Xu
 				}
@@ -186,6 +232,8 @@ void check_Mode()
 					chksum2 = ~(StartM + dataF1 + dataF2);
 				if (chksum == chksum2){
 					M_state = 5;
+					DataProtocol_SetAngPosi = (Set_AngPosi[0]*256) + Set_AngPosi[1];
+					AngPosi = (DataProtocol_SetAngPosi / (3.14 * 10000) * 180);
 					//HAL_UART_Transmit_DMA(&huart2, (uint8_t*)temp_s, 2);
 					HAL_UART_Transmit(&huart2, (uint8_t*)temp_s, 2 ,1000); //Xu
 				}
@@ -199,6 +247,7 @@ void check_Mode()
 					chksum2 = ~(StartM + dataF1 + dataF2);
 				if (chksum == chksum2){
 					M_state = 6;
+					Set1_Sta = Set_Goal_1Sta[1];
 					//HAL_UART_Transmit_DMA(&huart2, (uint8_t*)temp_s, 2);
 					HAL_UART_Transmit(&huart2, (uint8_t*)temp_s, 2 ,1000); //Xu
 				}
@@ -225,7 +274,7 @@ void check_Mode()
 					M_state = 8;
 					//HAL_UART_Transmit_DMA(&huart2, (uint8_t*)temp_s, 2);
 					HAL_UART_Transmit(&huart2, (uint8_t*)temp_s, 2 ,1000); //Xu
-					HAL_UART_Transmit(&huart2, (uint8_t*)temp_f, 2 ,1000);
+					//HAL_UART_Transmit(&huart2, (uint8_t*)temp_f, 2 ,1000);
 				}
 				break;
 			case 9: //10011001 01100110
@@ -235,6 +284,9 @@ void check_Mode()
 					M_state = 9;
 					//HAL_UART_Transmit_DMA(&huart2, (uint8_t*)temp_s, 2);
 					HAL_UART_Transmit(&huart2, (uint8_t*)temp_s, 2 ,1000);
+					//Req_sta[1] = 0;
+					//Req_sta[2] = x;
+					//Req_sta[3] = ~(Req_sta[0] + Req_sta[1] + Req_sta[2]);
 					HAL_UART_Transmit(&huart2, (uint8_t*)Req_sta, 4 ,1000);
 				}
 				break;
@@ -245,6 +297,10 @@ void check_Mode()
 					M_state = 10;
 					//HAL_UART_Transmit_DMA(&huart2, (uint8_t*)temp_s, 2);
 					HAL_UART_Transmit(&huart2, (uint8_t*)temp_s, 2 ,1000);
+					DataProtocol_AngPosi = (CurrentEn * 10000);
+					Req_AngPosi[1] = (DataProtocol_AngPosi / 256);
+					Req_AngPosi[2] = (DataProtocol_AngPosi % 256);
+					Req_AngPosi[3] = ~(Req_AngPosi[0] + Req_AngPosi[1] + Req_AngPosi[2]);
 					HAL_UART_Transmit(&huart2, (uint8_t*)Req_AngPosi, 4 ,1000);
 				}
 				break;
@@ -255,6 +311,10 @@ void check_Mode()
 					M_state = 11;
 					//HAL_UART_Transmit_DMA(&huart2, (uint8_t*)temp_s, 2);
 					HAL_UART_Transmit(&huart2, (uint8_t*)temp_s, 2 ,1000);
+					DataProtocol_Velo = (KalV/(2 * 3.14)) * 60;
+					Req_MaxVelo[1] = 0;
+					Req_MaxVelo[2] = (DataProtocol_Velo * 255) / 10;
+					Req_MaxVelo[3] = ~(Req_MaxVelo[0] + Req_MaxVelo[1] + Req_MaxVelo[2]);
 					HAL_UART_Transmit(&huart2, (uint8_t*)Req_MaxVelo, 4 ,1000);
 				}
 				break;
@@ -320,9 +380,12 @@ int main(void)
   MX_GPIO_Init();
   MX_DMA_Init();
   MX_USART2_UART_Init();
+  MX_TIM11_Init();
   /* USER CODE BEGIN 2 */
   HAL_UARTEx_ReceiveToIdle_DMA(&huart2, RxBuf, RxBuf_SIZE);
   __HAL_DMA_DISABLE_IT(&hdma_usart2_rx, DMA_IT_HT);
+  HAL_TIM_Base_Start_IT(&htim11);
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -333,6 +396,13 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+
+	  if(micros() - timestamp >= 100000){
+		  timestamp = micros();
+		  encdummbuf++;
+		  encdummbuf%=1023;
+		  CurrentEn = 0.006136 * encdummbuf;
+	  }
   }
   /* USER CODE END 3 */
 }
@@ -358,9 +428,9 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 16;
-  RCC_OscInitStruct.PLL.PLLN = 336;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 100;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 4;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
@@ -375,10 +445,41 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief TIM11 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM11_Init(void)
+{
+
+  /* USER CODE BEGIN TIM11_Init 0 */
+
+  /* USER CODE END TIM11_Init 0 */
+
+  /* USER CODE BEGIN TIM11_Init 1 */
+
+  /* USER CODE END TIM11_Init 1 */
+  htim11.Instance = TIM11;
+  htim11.Init.Prescaler = 99;
+  htim11.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim11.Init.Period = 65535;
+  htim11.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim11.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim11) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM11_Init 2 */
+
+  /* USER CODE END TIM11_Init 2 */
+
 }
 
 /**
@@ -466,6 +567,31 @@ static void MX_GPIO_Init(void)
 
 }
 
+/* USER CODE BEGIN 4 */
+//void AngPos_Dummy()
+//{
+//	if(micros()-timestamp > 500000){
+//		timestamp = micros();
+//		if (Ang_Position <= 360){
+//			Ang_Position =
+//			Ang_Position = Ang_Position + 5;
+//		}
+//
+//
+//	}
+//}
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+ if(htim == &htim11)
+ {
+	 _micros += 65535;
+ }
+}
+//TIM microsecond measure function Read time of _micros  + time of TIM11
+uint64_t micros()
+{
+	return _micros + htim11.Instance->CNT;
+}
 /* USER CODE END 4 */
 
 /**
